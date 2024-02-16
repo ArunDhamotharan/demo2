81703ec10c6d201a026b6911fd260f62618c09d5
ArunDhamotharan <119954096+ArunDhamotharan@users.noreply.github.com>
2024-02-16 20:37:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-print("Hello World!")
-a=3+7
-print(a)
+num = 7
+fact = 1
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       fact = fact*i
+   print(fact)
